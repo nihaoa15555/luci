a7a23838110e858629ef8b57d1a29cf3f5147bbb
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-24 12:04:16 +0800

    Update firewall.lua

--- applications/luci-app-firewall/luasrc/controller/firewall.lua
+++ applications/luci-app-firewall/luasrc/controller/firewall.lua
@@ -5,9 +5,9 @@ function index()
 		alias("admin", "network", "firewall", "zones"),
 		_("Firewall"), 60)
 
-	--entry({"admin", "network", "firewall", "zones"},
-		--arcombine(cbi("firewall/zones"), cbi("firewall/zone-details")),
-		--_("General Settings"), 10).leaf = true
+	entry({"admin", "network", "firewall", "zones"},
+		arcombine(cbi("firewall/zones"), cbi("firewall/zone-details")),
+		_("General Settings"), 10).leaf = true
 
 	--entry({"admin", "network", "firewall", "forwards"},
 		--arcombine(cbi("firewall/forwards"), cbi("firewall/forward-details")),
