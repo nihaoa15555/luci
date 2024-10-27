cd748473067bd5358b7b6c063c1452cadc90a17e
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-10-27 10:32:40 +0800

    Update system.lua

--- modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
@@ -1,430 +1,207 @@
 -- Copyright 2008 Steven Barth <steven@midlink.org>
--- Copyright 2008-2011 Jo-Philipp Wich <jow@openwrt.org>
+-- Copyright 2008 Jo-Philipp Wich <jow@openwrt.org>
 -- Licensed to the public under the Apache License 2.0.
 
 module("luci.controller.admin.system", package.seeall)
 
 function index()
-	local fs = require "nixio.fs"
-
-	entry({"admin", "system"}, alias("admin", "system", "system"), _("System"), 30).index = true
-	--entry({"admin", "system", "system"}, cbi("admin_system/system"), _("System"), 1)
-	--entry({"admin", "system", "clock_status"}, post_on({ set = true }, "action_clock_status"))
-
-	entry({"admin", "system", "admin"}, cbi("admin_system/admin"), _("Administration"), 2)
-
-	if fs.access("/bin/opkg") then
-		--entry({"admin", "system", "packages"}, post_on({ exec = "1" }, "action_packages"), _("Software"), 10)
-		--entry({"admin", "system", "packages", "ipkg"}, form("admin_system/ipkg"))
-	end
-
-	--entry({"admin", "system", "startup"}, form("admin_system/startup"), _("Startup"), 45)
-	--entry({"admin", "system", "crontab"}, form("admin_system/crontab"), _("Scheduled Tasks"), 46)
-
-	if fs.access("/sbin/block") and fs.access("/etc/config/fstab") then
-		--entry({"admin", "system", "fstab"}, cbi("admin_system/fstab"), _("Mount Points"), 50)
-		--entry({"admin", "system", "fstab", "mount"}, cbi("admin_system/fstab/mount"), nil).leaf = true
-		--entry({"admin", "system", "fstab", "swap"},  cbi("admin_system/fstab/swap"),  nil).leaf = true
-	end
-
-	local nodes, number = nixio.fs.glob("/sys/class/leds/*")
-	if number > 0 then
-		--entry({"admin", "system", "leds"}, cbi("admin_system/leds"), _("<abbr title=\"Light Emitting Diode\">LED</abbr> Configuration"), 60)
-	end
-
-	entry({"admin", "system", "flashops"}, call("action_flashops"), _("Backup / Flash Firmware"), 70)
-	--entry({"admin", "system", "flashops", "reset"}, post("action_reset"))
-	--entry({"admin", "system", "flashops", "backup"}, post("action_backup"))
-	--entry({"admin", "system", "flashops", "backupmtdblock"}, post("action_backupmtdblock"))
-	--entry({"admin", "system", "flashops", "backupfiles"}, form("admin_system/backupfiles"))
-
-	-- call() instead of post() due to upload handling!
-	entry({"admin", "system", "flashops", "restore"}, call("action_restore"))
-	entry({"admin", "system", "flashops", "sysupgrade"}, call("action_sysupgrade"))
-
-	entry({"admin", "system", "reboot"}, template("admin_system/reboot"), _("Reboot"), 90)
-	entry({"admin", "system", "reboot", "call"}, post("action_reboot"))
-end
-
-function action_clock_status()
-	local set = tonumber(luci.http.formvalue("set"))
-	if set ~= nil and set > 0 then
-		local date = os.date("*t", set)
-		if date then
-			luci.sys.call("date -s '%04d-%02d-%02d %02d:%02d:%02d'" %{
-				date.year, date.month, date.day, date.hour, date.min, date.sec
-			})
-			luci.sys.call("/etc/init.d/sysfixtime restart")
-		end
-	end
-
-	luci.http.prepare_content("application/json")
-	luci.http.write_json({ timestring = os.date("%c") })
-end
-
-function action_packages()
-	local fs = require "nixio.fs"
-	local ipkg = require "luci.model.ipkg"
-	local submit = (luci.http.formvalue("exec") == "1")
-	local update, upgrade
-	local changes = false
-	local install = { }
-	local remove  = { }
-	local stdout  = { "" }
-	local stderr  = { "" }
-	local out, err
-
-	-- Display
-	local display = luci.http.formvalue("display") or "installed"
-
-	-- Letter
-	local letter = string.byte(luci.http.formvalue("letter") or "A", 1)
-	letter = (letter == 35 or (letter >= 65 and letter <= 90)) and letter or 65
-
-	-- Search query
-	local query = luci.http.formvalue("query")
-	query = (query ~= '') and query or nil
-
-
-	-- Modifying actions
-	if submit then
-		-- Packets to be installed
-		local ninst = luci.http.formvalue("install")
-		local uinst = nil
-
-		-- Install from URL
-		local url = luci.http.formvalue("url")
-		if url and url ~= '' then
-			uinst = url
-		end
-
-		-- Do install
-		if ninst then
-			install[ninst], out, err = ipkg.install(ninst)
-			stdout[#stdout+1] = out
-			stderr[#stderr+1] = err
-			changes = true
-		end
-
-		if uinst then
-			local pkg
-			for pkg in luci.util.imatch(uinst) do
-				install[uinst], out, err = ipkg.install(pkg)
-				stdout[#stdout+1] = out
-				stderr[#stderr+1] = err
-				changes = true
-			end
-		end
-
-		-- Remove packets
-		local rem = luci.http.formvalue("remove")
-		if rem then
-			remove[rem], out, err = ipkg.remove(rem)
-			stdout[#stdout+1] = out
-			stderr[#stderr+1] = err
-			changes = true
-		end
-
-
-		-- Update all packets
-		update = luci.http.formvalue("update")
-		if update then
-			update, out, err = ipkg.update()
-			stdout[#stdout+1] = out
-			stderr[#stderr+1] = err
-		end
-
-
-		-- Upgrade all packets
-		upgrade = luci.http.formvalue("upgrade")
-		if upgrade then
-			upgrade, out, err = ipkg.upgrade()
-			stdout[#stdout+1] = out
-			stderr[#stderr+1] = err
-		end
-	end
-
-
-	-- List state
-	local no_lists = true
-	local old_lists = false
-	if fs.access("/var/opkg-lists/") then
-		local list
-		for list in fs.dir("/var/opkg-lists/") do
-			no_lists = false
-			if (fs.stat("/var/opkg-lists/"..list, "mtime") or 0) < (os.time() - (24 * 60 * 60)) then
-				old_lists = true
-				break
-			end
-		end
-	end
-
-
-	luci.template.render("admin_system/packages", {
-		display   = display,
-		letter    = letter,
-		query     = query,
-		install   = install,
-		remove    = remove,
-		update    = update,
-		upgrade   = upgrade,
-		no_lists  = no_lists,
-		old_lists = old_lists,
-		stdout    = table.concat(stdout, ""),
-		stderr    = table.concat(stderr, "")
-	})
-
-	-- Remove index cache
-	if changes then
-		fs.unlink("/tmp/luci-indexcache")
-	end
-end
-
-local function image_supported(image)
-	return (os.execute("sysupgrade -T %q >/dev/null" % image) == 0)
-end
-
-local function image_checksum(image)
-	return (luci.sys.exec("md5sum %q" % image):match("^([^%s]+)"))
-end
-
-local function image_sha256_checksum(image)
-	return (luci.sys.exec("sha256sum %q" % image):match("^([^%s]+)"))
-end
-
-local function supports_sysupgrade()
-	return nixio.fs.access("/lib/upgrade/platform.sh")
-end
-
-local function supports_reset()
-	return (os.execute([[grep -sq "^overlayfs:/overlay / overlay " /proc/mounts]]) == 0)
-end
-
-local function storage_size()
-	local size = 0
-	if nixio.fs.access("/proc/mtd") then
-		for l in io.lines("/proc/mtd") do
-			local d, s, e, n = l:match('^([^%s]+)%s+([^%s]+)%s+([^%s]+)%s+"([^%s]+)"')
-			if n == "linux" or n == "firmware" then
-				size = tonumber(s, 16)
-				break
-			end
-		end
-	elseif nixio.fs.access("/proc/partitions") then
-		for l in io.lines("/proc/partitions") do
-			local x, y, b, n = l:match('^%s*(%d+)%s+(%d+)%s+([^%s]+)%s+([^%s]+)')
-			if b and n and not n:match('[0-9]') then
-				size = tonumber(b) * 1024
-				break
-			end
-		end
-	end
-	return size
-end
-
-
-function action_flashops()
-	--
-	-- Overview
-	--
-	luci.template.render("admin_system/flashops", {
-		reset_avail   = supports_reset(),
-		upgrade_avail = supports_sysupgrade()
-	})
-end
-
-function action_sysupgrade()
-	local fs = require "nixio.fs"
-	local http = require "luci.http"
-	local image_tmp = "/tmp/firmware.img"
-
-	local fp
-	http.setfilehandler(
-		function(meta, chunk, eof)
-			if not fp and meta and meta.name == "image" then
-				fp = io.open(image_tmp, "w")
-			end
-			if fp and chunk then
-				fp:write(chunk)
-			end
-			if fp and eof then
-				fp:close()
-			end
-		end
-	)
-
-	if not luci.dispatcher.test_post_security() then
-		fs.unlink(image_tmp)
-		return
-	end
-
-	--
-	-- Cancel firmware flash
-	--
-	if http.formvalue("cancel") then
-		fs.unlink(image_tmp)
-		http.redirect(luci.dispatcher.build_url('admin/system/flashops'))
-		return
-	end
-
-	--
-	-- Initiate firmware flash
-	--
-	local step = tonumber(http.formvalue("step") or 1)
-	if step == 1 then
-		if image_supported(image_tmp) then
-			luci.template.render("admin_system/upgrade", {
-				checksum = image_checksum(image_tmp),
-				sha256ch = image_sha256_checksum(image_tmp),
-				storage  = storage_size(),
-				size     = (fs.stat(image_tmp, "size") or 0),
-				keep     = (not not http.formvalue("keep"))
-			})
-		else
-			fs.unlink(image_tmp)
-			luci.template.render("admin_system/flashops", {
-				reset_avail   = supports_reset(),
-				upgrade_avail = supports_sysupgrade(),
-				image_invalid = true
-			})
-		end
-	--
-	-- Start sysupgrade flash
-	--
-	elseif step == 2 then
-		local keep = (http.formvalue("keep") == "1") and "" or "-n"
-		luci.template.render("admin_system/applyreboot", {
-			title = luci.i18n.translate("Flashing..."),
-			msg   = luci.i18n.translate("The system is flashing now.<br /> DO NOT POWER OFF THE DEVICE!<br /> Wait a few minutes before you try to reconnect. It might be necessary to renew the address of your computer to reach the device again, depending on your settings."),
-			addr  = (#keep > 0) and "192.168.1.1" or nil
-		})
-		fork_exec("sleep 1; killall dropbear uhttpd; sleep 1; /sbin/sysupgrade %s %q" %{ keep, image_tmp })
-	end
+	entry({"admin", "system"}, alias("admin", "system", "index"), _("System"), 40).index = true
+	entry({"admin", "system", "index"}, cbi("admin/system", {autoapply=true}), _("General"), 1)
+	entry({"admin", "system", "passwd"}, form("admin/passwd"), _("Admin Password"), 10)
+	entry({"admin", "system", "backup"}, call("action_backup"), _("Backup / Restore"), 80)
+	entry({"admin", "system", "upgrade"}, call("action_upgrade"), _("Flash Firmware"), 90)
+	entry({"admin", "system", "reboot"}, call("action_reboot"), _("Reboot"), 100)
 end
 
 function action_backup()
-	local reader = ltn12_popen("sysupgrade --create-backup - 2>/dev/null")
-
-	luci.http.header(
-		'Content-Disposition', 'attachment; filename="backup-%s-%s.tar.gz"' %{
-			luci.sys.hostname(),
-			os.date("%Y-%m-%d")
-		})
-
-	luci.http.prepare_content("application/x-targz")
-	luci.ltn12.pump.all(reader, luci.http.write)
-end
-
-function action_backupmtdblock()
-	local http = require "luci.http"
-	local mv = http.formvalue("mtdblockname")
-	local m, s, n = mv:match('^([^%s]+)/([^%s]+)/([^%s]+)')
-
-	local reader = ltn12_popen("cat /dev/mtd%s" % n)
-
-	luci.http.header(
-		'Content-Disposition', 'attachment; filename="backup-%s-%s-%s.bin"' %{
-			luci.sys.hostname(), m,
-			os.date("%Y-%m-%d")
-		})
-
-	luci.http.prepare_content("application/octet-stream")
-	luci.ltn12.pump.all(reader, luci.http.write)
-end
-
-function action_restore()
-	local fs = require "nixio.fs"
-	local http = require "luci.http"
-	local archive_tmp = "/tmp/restore.tar.gz"
-
-	local fp
-	http.setfilehandler(
+	local reset_avail = os.execute([[grep '"rootfs_data"' /proc/mtd >/dev/null 2>&1]]) == 0
+	local restore_cmd = "gunzip | tar -xC/ >/dev/null 2>&1"
+	local backup_cmd  = "tar -c %s | gzip 2>/dev/null"
+	
+	local restore_fpi 
+	luci.http.setfilehandler(
 		function(meta, chunk, eof)
-			if not fp and meta and meta.name == "archive" then
-				fp = io.open(archive_tmp, "w")
+			if not restore_fpi then
+				restore_fpi = io.popen(restore_cmd, "w")
 			end
-			if fp and chunk then
-				fp:write(chunk)
+			if chunk then
+				restore_fpi:write(chunk)
 			end
-			if fp and eof then
-				fp:close()
+			if eof then
+				restore_fpi:close()
+			end
+		end
+	)
+		  
+	local upload = luci.http.formvalue("archive")
+	local backup = luci.http.formvalue("backup")
+	local reset  = reset_avail and luci.http.formvalue("reset")
+	
+	if upload and #upload > 0 then
+		luci.template.render("admin/applyreboot")
+		luci.sys.reboot()
+	elseif backup then
+		local reader = ltn12_popen(backup_cmd:format(_keep_pattern()))
+		luci.http.header('Content-Disposition', 'attachment; filename="backup-%s-%s.tar.gz"' % {
+			luci.sys.hostname(), os.date("%Y-%m-%d")})
+		luci.http.prepare_content("application/x-targz")
+		luci.ltn12.pump.all(reader, luci.http.write)
+	elseif reset then
+		luci.template.render("admin/applyreboot")
+		luci.util.exec("mtd -r erase rootfs_data")
+	else
+		luci.template.render("admin/backup", {reset_avail = reset_avail})
+	end
+end
+
+function action_reboot()
+	local reboot = luci.http.formvalue("reboot")
+	luci.template.render("admin/reboot", {reboot=reboot})
+	if reboot then
+		luci.sys.reboot()
+	end
+end
+
+function action_upgrade()
+	require("luci.model.uci")
+
+	local tmpfile = "/tmp/firmware.img"
+	
+	local function image_supported()
+		-- XXX: yay...
+		return ( 0 == os.execute(
+			". /lib/functions.sh; " ..
+			"include /lib/upgrade; " ..
+			"platform_check_image %q >/dev/null"
+				% tmpfile
+		) )
+	end
+	
+	local function image_checksum()
+		return (luci.sys.exec("md5sum %q" % tmpfile):match("^([^%s]+)"))
+	end
+	
+	local function storage_size()
+		local size = 0
+		if nixio.fs.access("/proc/mtd") then
+			for l in io.lines("/proc/mtd") do
+				local d, s, e, n = l:match('^([^%s]+)%s+([^%s]+)%s+([^%s]+)%s+"([^%s]+)"')
+				if n == "linux" then
+					size = tonumber(s, 16)
+					break
+				end
+			end
+		elseif nixio.fs.access("/proc/partitions") then
+			for l in io.lines("/proc/partitions") do
+				local x, y, b, n = l:match('^%s*(%d+)%s+(%d+)%s+([^%s]+)%s+([^%s]+)')
+				if b and n and not n:match('[0-9]') then
+					size = tonumber(b) * 1024
+					break
+				end
+			end
+		end
+		return size
+	end
+
+
+	-- Install upload handler
+	local file
+	luci.http.setfilehandler(
+		function(meta, chunk, eof)
+			if not nixio.fs.access(tmpfile) and not file and chunk and #chunk > 0 then
+				file = io.open(tmpfile, "w")
+			end
+			if file and chunk then
+				file:write(chunk)
+			end
+			if file and eof then
+				file:close()
 			end
 		end
 	)
 
-	if not luci.dispatcher.test_post_security() then
-		fs.unlink(archive_tmp)
-		return
-	end
 
-	local upload = http.formvalue("archive")
-	if upload and #upload > 0 then
-		luci.template.render("admin_system/applyreboot")
-		os.execute("tar -C / -xzf %q >/dev/null 2>&1" % archive_tmp)
-		luci.sys.reboot()
-		return
-	end
+	-- Determine state
+	local keep_avail   = true
+	local step         = tonumber(luci.http.formvalue("step") or 1)
+	local has_image    = nixio.fs.access(tmpfile)
+	local has_support  = image_supported()
+	local has_platform = nixio.fs.access("/lib/upgrade/platform.sh")
+	local has_upload   = luci.http.formvalue("image")
+	
+	-- This does the actual flashing which is invoked inside an iframe
+	-- so don't produce meaningful errors here because the the 
+	-- previous pages should arrange the stuff as required.
+	if step == 4 then
+		if has_platform and has_image and has_support then
+			-- Mimetype text/plain
+			luci.http.prepare_content("text/plain")
+			luci.http.write("Starting luci-flash...\n")
 
-	http.redirect(luci.dispatcher.build_url('admin/system/flashops'))
-end
+			-- Now invoke sysupgrade
+			local keepcfg = keep_avail and luci.http.formvalue("keepcfg") == "1"
+			local flash = ltn12_popen("/sbin/luci-flash %s %q" %{
+				keepcfg and "-k %q" % _keep_pattern() or "", tmpfile
+			})
 
-function action_reset()
-	if supports_reset() then
-		luci.template.render("admin_system/applyreboot", {
-			title = luci.i18n.translate("Erasing..."),
-			msg   = luci.i18n.translate("The system is erasing the configuration partition now and will reboot itself when finished."),
-			addr  = "192.168.1.1"
-		})
+			luci.ltn12.pump.all(flash, luci.http.write)
 
-		fork_exec("sleep 1; killall dropbear uhttpd; sleep 1; jffs2reset -y && reboot")
-		return
-	end
-
-	http.redirect(luci.dispatcher.build_url('admin/system/flashops'))
-end
-
-function action_passwd()
-	local p1 = luci.http.formvalue("pwd1")
-	local p2 = luci.http.formvalue("pwd2")
-	local stat = nil
-
-	if p1 or p2 then
-		if p1 == p2 then
-			stat = luci.sys.user.setpasswd("root", p1)
-		else
-			stat = 10
+			-- Make sure the device is rebooted
+			luci.sys.reboot()
 		end
-	end
 
-	luci.template.render("admin_system/passwd", {stat=stat})
+
+	--
+	-- This is step 1-3, which does the user interaction and
+	-- image upload.
+	--
+
+	-- Step 1: file upload, error on unsupported image format
+	elseif not has_image or not has_support or step == 1 then
+		-- If there is an image but user has requested step 1
+		-- or type is not supported, then remove it.
+		if has_image then
+			nixio.fs.unlink(tmpfile)
+		end
+			
+		luci.template.render("admin/upgrade", {
+			step=1,
+			bad_image=(has_image and not has_support or false),
+			keepavail=keep_avail,
+			supported=has_platform
+		} )
+
+	-- Step 2: present uploaded file, show checksum, confirmation
+	elseif step == 2 then
+		luci.template.render("admin/upgrade", {
+			step=2,
+			checksum=image_checksum(),
+			filesize=nixio.fs.stat(tmpfile).size,
+			flashsize=storage_size(),
+			keepconfig=(keep_avail and luci.http.formvalue("keepcfg") == "1")
+		} )
+	
+	-- Step 3: load iframe which calls the actual flash procedure
+	elseif step == 3 then
+		luci.template.render("admin/upgrade", {
+			step=3,
+			keepconfig=(keep_avail and luci.http.formvalue("keepcfg") == "1")
+		} )
+	end	
 end
 
-function action_reboot()
-	luci.sys.reboot()
-end
-
-function fork_exec(command)
-	local pid = nixio.fork()
-	if pid > 0 then
-		return
-	elseif pid == 0 then
-		-- change to root dir
-		nixio.chdir("/")
-
-		-- patch stdin, out, err to /dev/null
-		local null = nixio.open("/dev/null", "w+")
-		if null then
-			nixio.dup(null, nixio.stderr)
-			nixio.dup(null, nixio.stdout)
-			nixio.dup(null, nixio.stdin)
-			if null:fileno() > 2 then
-				null:close()
+function _keep_pattern()
+	local kpattern = ""
+	local files = luci.model.uci.cursor():get_all("luci", "flash_keep")
+	if files then
+		kpattern = ""
+		for k, v in pairs(files) do
+			if k:sub(1,1) ~= "." and nixio.fs.glob(v)() then
+				kpattern = kpattern .. " " ..  v
 			end
 		end
-
-		-- replace with target command
-		nixio.exec("/bin/sh", "-c", command)
 	end
+	return kpattern
 end
 
 function ltn12_popen(command)
