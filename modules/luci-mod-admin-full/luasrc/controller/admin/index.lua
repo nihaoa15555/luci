dc087e9d4d3f0521b1be718677ac1af1553cb5f4
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-10-27 10:34:22 +0800

    Update index.lua

--- modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
@@ -1,46 +1,39 @@
 -- Copyright 2008 Steven Barth <steven@midlink.org>
+-- Copyright 2008 Jo-Philipp Wich <jow@openwrt.org>
 -- Licensed to the public under the Apache License 2.0.
 
 module("luci.controller.admin.index", package.seeall)
 
 function index()
 	local root = node()
-	if not root.target then
+	if not root.lock then
 		root.target = alias("admin")
 		root.index = true
 	end
-
-	local page   = node("admin")
-	page.target  = firstchild()
-	page.title   = _("Administration")
-	page.order   = 10
+	
+	entry({"about"}, template("about"))
+	
+	local page   = entry({"admin"}, alias("admin", "index"), _("Essentials"), 10)
 	page.sysauth = "root"
 	page.sysauth_authenticator = "htmlauth"
-	page.ucidata = true
 	page.index = true
-
-	-- Empty services menu to be populated by addons
-	entry({"admin", "services"}, firstchild(), _("Services"), 40).index = true
 	
-	entry({"admin", "nas"}, firstchild(), _("NAS"), 44).index = true
-	
-	entry({"admin", "vpn"}, firstchild(), _("VPN"), 44).index = true
-
-	entry({"admin", "logout"}, call("action_logout"), _("Logout"), 90)
+	entry({"admin", "index"}, alias("admin", "index", "index"), _("Overview"), 10).index = true
+	entry({"admin", "index", "index"}, form("admin/index"), _("General"), 1).ignoreindex = true
+	entry({"admin", "index", "luci"}, cbi("admin/luci", {autoapply=true}), _("Settings"), 10)
+	entry({"admin", "index", "logout"}, call("action_logout"), _("Logout"))
 end
 
 function action_logout()
 	local dsp = require "luci.dispatcher"
 	local utl = require "luci.util"
-	local sid = dsp.context.authsession
-
-	if sid then
-		utl.ubus("session", "destroy", { ubus_rpc_session = sid })
-
-		luci.http.header("Set-Cookie", "sysauth=%s; expires=%s; path=%s/" %{
-			sid, 'Thu, 01 Jan 1970 01:00:00 GMT', dsp.build_url()
+	if dsp.context.authsession then
+		utl.ubus("session", "destroy", {
+			ubus_rpc_session = dsp.context.authsession
 		})
+		dsp.context.urltoken.stok = nil
 	end
 
-	luci.http.redirect(dsp.build_url())
+	luci.http.header("Set-Cookie", "sysauth=; path=" .. dsp.build_url())
+	luci.http.redirect(luci.dispatcher.build_url())
 end
