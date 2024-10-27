4284b185564e28c2023a71fcf476d5f85f2ec1ae
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-10-27 10:36:59 +0800

    Update status.lua

--- modules/luci-mod-admin-full/luasrc/controller/admin/status.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/status.lua
@@ -2,18 +2,18 @@
 -- Copyright 2011 Jo-Philipp Wich <jow@openwrt.org>
 -- Licensed to the public under the Apache License 2.0.
 
-module("luci.controller.admin.status", package.seeall)
+--module("luci.controller.admin.status", package.seeall)
 
-function index()
-	entry({"admin", "status"}, alias("admin", "status", "overview"), _("Status"), 20).index = true
-	entry({"admin", "status", "overview"}, template("admin_status/index"), _("Overview"), 1)
+--function index()
+	--entry({"admin", "status"}, alias("admin", "status", "overview"), _("Status"), 20).index = true
+	--entry({"admin", "status", "overview"}, template("admin_status/index"), _("Overview"), 1)
 
-	entry({"admin", "status", "iptables"}, template("admin_status/iptables"), _("Firewall"), 2).leaf = true
-	entry({"admin", "status", "iptables_action"}, post("action_iptables")).leaf = true
-	entry({"admin", "status", "free_memory_action"}, post("action_free_memory")).leaf = true
+	--entry({"admin", "status", "iptables"}, template("admin_status/iptables"), _("Firewall"), 2).leaf = true
+	--entry({"admin", "status", "iptables_action"}, post("action_iptables")).leaf = true
+	--entry({"admin", "status", "free_memory_action"}, post("action_free_memory")).leaf = true
 	
 	--entry({"admin", "status", "routes"}, template("admin_status/routes"), _("Routes"), 3)
-	entry({"admin", "status", "syslog"}, call("action_syslog"), _("System Log"), 4)
+	--entry({"admin", "status", "syslog"}, call("action_syslog"), _("System Log"), 4)
 	--entry({"admin", "status", "dmesg"}, call("action_dmesg"), _("Kernel Log"), 5)
 	--entry({"admin", "status", "processes"}, cbi("admin_status/processes"), _("Processes"), 6)
 
@@ -34,127 +34,127 @@ function index()
 	--entry({"admin", "status", "realtime", "connections_status"}, call("action_connections")).leaf = true
 
 	--entry({"admin", "status", "nameinfo"}, call("action_nameinfo")).leaf = true
-end
+--end
 
-function action_syslog()
-	local syslog = luci.sys.syslog()
-	luci.template.render("admin_status/syslog", {syslog=syslog})
-end
+--function action_syslog()
+	--local syslog = luci.sys.syslog()
+	--luci.template.render("admin_status/syslog", {syslog=syslog})
+--end
 
-function action_dmesg()
-	local dmesg = luci.sys.dmesg()
-	luci.template.render("admin_status/dmesg", {dmesg=dmesg})
-end
+--function action_dmesg()
+	--local dmesg = luci.sys.dmesg()
+	--luci.template.render("admin_status/dmesg", {dmesg=dmesg})
+--end
 
-function action_iptables()
-	if luci.http.formvalue("zero") then
-		if luci.http.formvalue("family") == "6" then
+--function action_iptables()
+	--if luci.http.formvalue("zero") then
+		--if luci.http.formvalue("family") == "6" then
 			luci.util.exec("/usr/sbin/ip6tables -Z")
-		else
-			luci.util.exec("/usr/sbin/iptables -Z")
-		end
-	elseif luci.http.formvalue("restart") then
-		luci.util.exec("/etc/init.d/firewall restart")
-	end
+		--else
+			--luci.util.exec("/usr/sbin/iptables -Z")
+		--end
+	--elseif luci.http.formvalue("restart") then
+		--luci.util.exec("/etc/init.d/firewall restart")
+	--end
 
-	luci.http.redirect(luci.dispatcher.build_url("admin/status/iptables"))
-end
+	--luci.http.redirect(luci.dispatcher.build_url("admin/status/iptables"))
+--end
 
-function action_free_memory()
-	if luci.http.formvalue("free") then
-		luci.util.exec("/bin/sync")
-		luci.util.exec("/bin/echo 3 > /proc/sys/vm/drop_caches")
-	end
+--function action_free_memory()
+	--if luci.http.formvalue("free") then
+		--luci.util.exec("/bin/sync")
+		--luci.util.exec("/bin/echo 3 > /proc/sys/vm/drop_caches")
+	--end
 
-	luci.http.redirect(luci.dispatcher.build_url("admin/status"))
-end
+	--luci.http.redirect(luci.dispatcher.build_url("admin/status"))
+--end
 
-function action_bandwidth(iface)
-	luci.http.prepare_content("application/json")
+--function action_bandwidth(iface)
+	--luci.http.prepare_content("application/json")
 
-	local bwc = io.popen("luci-bwc -i %q 2>/dev/null" % iface)
-	if bwc then
-		luci.http.write("[")
+	--local bwc = io.popen("luci-bwc -i %q 2>/dev/null" % iface)
+	--if bwc then
+		--luci.http.write("[")
 
-		while true do
-			local ln = bwc:read("*l")
-			if not ln then break end
-			luci.http.write(ln)
-		end
+		--while true do
+			--local ln = bwc:read("*l")
+			--if not ln then break end
+			--luci.http.write(ln)
+		--end
 
-		luci.http.write("]")
-		bwc:close()
-	end
-end
+		--luci.http.write("]")
+		--bwc:close()
+	--end
+--end
 
-function action_wireless(iface)
-	luci.http.prepare_content("application/json")
+--function action_wireless(iface)
+	--luci.http.prepare_content("application/json")
 
-	local bwc = io.popen("luci-bwc -r %q 2>/dev/null" % iface)
-	if bwc then
-		luci.http.write("[")
+	--local bwc = io.popen("luci-bwc -r %q 2>/dev/null" % iface)
+	--if bwc then
+		--luci.http.write("[")
 
-		while true do
-			local ln = bwc:read("*l")
-			if not ln then break end
-			luci.http.write(ln)
-		end
+		--while true do
+			--local ln = bwc:read("*l")
+			--if not ln then break end
+			--luci.http.write(ln)
+		--end
 
-		luci.http.write("]")
-		bwc:close()
-	end
-end
+		--luci.http.write("]")
+		--bwc:close()
+	--end
+--end
 
-function action_load()
-	luci.http.prepare_content("application/json")
+--function action_load()
+	--luci.http.prepare_content("application/json")
 
-	local bwc = io.popen("luci-bwc -l 2>/dev/null")
-	if bwc then
-		luci.http.write("[")
+	--local bwc = io.popen("luci-bwc -l 2>/dev/null")
+	--if bwc then
+		--luci.http.write("[")
 
-		while true do
-			local ln = bwc:read("*l")
-			if not ln then break end
-			luci.http.write(ln)
-		end
+		--while true do
+			--local ln = bwc:read("*l")
+			--if not ln then break end
+			--luci.http.write(ln)
+		--end
 
-		luci.http.write("]")
-		bwc:close()
-	end
-end
+		--luci.http.write("]")
+		--bwc:close()
+	--end
+--end
 
-function action_connections()
-	local sys = require "luci.sys"
+--function action_connections()
+	--local sys = require "luci.sys"
 
-	luci.http.prepare_content("application/json")
+	--luci.http.prepare_content("application/json")
 
-	luci.http.write("{ connections: ")
-	luci.http.write_json(sys.net.conntrack())
+	--luci.http.write("{ connections: ")
+	--luci.http.write_json(sys.net.conntrack())
 
-	local bwc = io.popen("luci-bwc -c 2>/dev/null")
-	if bwc then
-		luci.http.write(", statistics: [")
+	--local bwc = io.popen("luci-bwc -c 2>/dev/null")
+	--if bwc then
+		--luci.http.write(", statistics: [")
 
-		while true do
-			local ln = bwc:read("*l")
-			if not ln then break end
-			luci.http.write(ln)
-		end
+		--while true do
+			--local ln = bwc:read("*l")
+			--if not ln then break end
+			--luci.http.write(ln)
+		--end
 
-		luci.http.write("]")
-		bwc:close()
-	end
+		--luci.http.write("]")
+		--bwc:close()
+	--end
 
-	luci.http.write(" }")
-end
+	--luci.http.write(" }")
+--end
 
-function action_nameinfo(...)
-	local util = require "luci.util"
+--function action_nameinfo(...)
+	--local util = require "luci.util"
 
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(util.ubus("network.rrdns", "lookup", {
-		addrs = { ... },
-		timeout = 5000,
-		limit = 1000
-	}) or { })
-end
+	--luci.http.prepare_content("application/json")
+	--luci.http.write_json(util.ubus("network.rrdns", "lookup", {
+		--addrs = { ... },
+		--timeout = 5000,
+		--limit = 1000
+	--}) or { })
+--end
