e4f51540ca34536af114d4694390683a2562c188
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2025-02-24 12:14:01 +0800

    Update status.lua

--- modules/luci-mod-admin-full/luasrc/controller/admin/status.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/status.lua
@@ -5,12 +5,12 @@
 module("luci.controller.admin.status", package.seeall)
 
 function index()
-	entry({"admin", "status"}, alias("admin", "status", "overview"), _("Status"), 20).index = true
-	entry({"admin", "status", "overview"}, template("admin_status/index"), _("Overview"), 1)
+	--entry({"admin", "status"}, alias("admin", "status", "overview"), _("Status"), 20).index = true
+	--entry({"admin", "status", "overview"}, template("admin_status/index"), _("Overview"), 1)
 
-	entry({"admin", "status", "iptables"}, template("admin_status/iptables"), _("Firewall"), 2).leaf = true
-	entry({"admin", "status", "iptables_action"}, post("action_iptables")).leaf = true
-	entry({"admin", "status", "free_memory_action"}, post("action_free_memory")).leaf = true
+	--entry({"admin", "status", "iptables"}, template("admin_status/iptables"), _("Firewall"), 2).leaf = true
+	--entry({"admin", "status", "iptables_action"}, post("action_iptables")).leaf = true
+	--entry({"admin", "status", "free_memory_action"}, post("action_free_memory")).leaf = true
 	
 	--entry({"admin", "status", "routes"}, template("admin_status/routes"), _("Routes"), 3)
 	--entry({"admin", "status", "syslog"}, call("action_syslog"), _("System Log"), 4)
