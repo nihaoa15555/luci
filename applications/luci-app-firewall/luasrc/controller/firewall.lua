dc5f28c9473055a466affc13fd894ede5af883e4
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-11 21:02:36 +0800

    Update firewall.lua

--- applications/luci-app-firewall/luasrc/controller/firewall.lua
+++ applications/luci-app-firewall/luasrc/controller/firewall.lua
@@ -1,13 +1,13 @@
 module("luci.controller.firewall", package.seeall)
 
 function index()
-	entry({"admin", "network", "firewall"},
-		alias("admin", "network", "firewall", "zones"),
-		_("Firewall"), 60)
+	--entry({"admin", "network", "firewall"},
+		--alias("admin", "network", "firewall", "zones"),
+		--_("Firewall"), 60)
 
-	entry({"admin", "network", "firewall", "zones"},
-		arcombine(cbi("firewall/zones"), cbi("firewall/zone-details")),
-		_("General Settings"), 10).leaf = true
+	--entry({"admin", "network", "firewall", "zones"},
+		--arcombine(cbi("firewall/zones"), cbi("firewall/zone-details")),
+		--_("General Settings"), 10).leaf = true
 
 	--entry({"admin", "network", "firewall", "forwards"},
 		--arcombine(cbi("firewall/forwards"), cbi("firewall/forward-details")),
